a64270c4076e60622bc2824755a1e836bea641e5
Joseph Davis <joethebartenderpdx@gmail.com>
2021-04-08 17:03:25 -0700

    Almost finished with tests

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 ## Following Dani's plan
 1) Do data modeling
 2) utils time! (findById, local-storage-utils)
-3) HTML setup!!!
-4) Login page to get user data into local storage
-5) Maps page with links to the quests 
-6) Build out the quest pages (render to page, update local storage on submit, redirect back to map)
-7) Back on maps page, conditionally render links and check if game is over, if so redirect to results page
-8) Design results page
+3) Test funcs womp womp
+4) HTML setup!!!
+5) Login page to get user data into local storage
+6) Maps page with links to the quests 
+7) Build out the quest pages (render to page, update local storage on submit, redirect back to map)
+8) Back on maps page, conditionally render links and check if game is over, if so redirect to results page
+9) Design results page
---
+++ test/local-storage-utils.test.js
@@ -0,0 +1,113 @@
+import { getUser, setUser, updateUserChoice } from '../local-storage-utils.js';
+
+
+const test = QUnit.test;
+
+test('should get the user', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const user = {
+        hp: 50,
+        gold: 0,
+        name: 'mamba',
+        class: 'dog',
+        completed: {}
+    };
+
+    localStorage.setItem('USER', JSON.stringify(user));
+
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getUser();
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.deepEqual(actual, user);
+});
+
+test('should set the user into local storage', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const user = {
+        hp: 50,
+        gold: 0,
+        name: 'mamba',
+        class: 'dog',
+        completed: {}
+    };
+
+    setUser(user);
+
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = JSON.parse(localStorage.getItem('USER'));
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.deepEqual(actual, user);
+});
+
+test('should get the user', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const user = {
+        hp: 50,
+        gold: 0,
+        name: 'mamba',
+        class: 'dog',
+        completed: {}
+    };
+
+    localStorage.setItem('USER', JSON.stringify(user));
+
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getUser();
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.deepEqual(actual, user);
+});
+
+test('should update user values', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const user = {
+        hp: 50,
+        gold: 0,
+        name: 'mamba',
+        class: 'dog',
+        completed: {}
+    };
+
+    localStorage.setItem('USER', JSON.stringify(user));
+
+    const choice = {
+        hp: 25,
+        gold: 3
+    };
+
+    const expected = {
+        hp: 75,
+        gold: 3,
+        name: 'mamba',
+        class: 'dog',
+        completed: {
+            dragon: true
+        }
+    };
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    updateUserChoice('dragon', choice);
+
+    const actual = JSON.parse(localStorage.getItem('USER'));
+    console.log('USER');
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.deepEqual(actual, expected);
+});
