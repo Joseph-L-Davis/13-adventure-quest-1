a003732577ba90502feae04eaf812dec4ba332d4
Joseph Davis <joethebartenderpdx@gmail.com>
2021-04-09 13:24:36 -0700

    added background to quest

--- data.js
+++ data.js
@@ -1,33 +1,27 @@
 const monsters = {
     id: 'monsters',
-    title: 'A Den of Monsters',
+    title: 'The Monster Squad',
     map: {
         top: '89%',
         left: '44%'
     },
     image: 'monsters.jpg',
     description: `
-        You enter the quest chamber only to be confronted by a hoard of
-        monsters. And they look hungry. What do you do?
+        Some foos run up on you demanding you join their monster squad or face fists... What do you do?
     `,
     choices: [{
         id: 'negotiate',
-        description: 'Negotiate with them',
+        description: 'Bargain with them',
         result: `
-            Knowing the monsters are not too bright, you offer to go buy them all
-            turkey dinners from the village pub. They give you 35 gold for meals
-            that will never be delivered. I hope you can live with yourself. 
+            Tell them you hard of hearing and your glasses just broke, and although you cant bring anything to the proverbial table you CAN point them in the direction of some foos that been talking smack. They reward you with a couple coins and one says "go get you a cheeseburger my G, you did good'. 
         `,
         hp: 0,
         gold: 35
     }, {
         id: 'fight',
-        description: 'Fiiiiiggghhhttt!',
+        description: 'Time to throw them hands!!!',
         result: `
-            Brandishing your sword you let out a warrior's cry and charge into the monsters
-            hacking and slashing. Before long you stand panting gazing across the bodies of
-            your vanquished foes. The bad news is you take 30 hp damage. The good news is you
-            find 50 gold.
+            You fake like you're about to succumb to whatever demands they have when all of a sudden...POW! You hit the largest foo right in the kisser like you're tyson fighting the rain and he goes down! The rest of the squad is impressed but they still stomp you out, holding back ever so slightly because you showed heart.
         `,
         hp: -30,
         gold: 50
--- quests/quest.js
+++ quests/quest.js
@@ -1,6 +1,6 @@
 import quests from '../data.js';
 import { findById } from '../utils.js';
-import { getUser, setUser, updateUserChoice } from '../local-storage-utils';
+import { getUser } from '../local-storage-utils';
 
 const section = document.querySelector('section');
 const params = new URLSearchParams(window.location.search);
@@ -9,3 +9,16 @@ const questId = params.get('id');
 const quest = findById(quests, questId);
 
 const h2 = document.createElement('h2');
+h2.textContent = quest.title;
+
+const image = document.createElement('img');
+image.src = quest.image;
+
+for (let choice of quest.choices) {
+    const label = document.createElement('label');
+    const radio = document.createElement('input');
+    radio.type = 'radio';
+    radio.name = 'choice';
+    radio.value = quest.id;
+    label.append(choice.description, radio);
+}
