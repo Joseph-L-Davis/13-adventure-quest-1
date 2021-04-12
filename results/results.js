2fc35fb37ebb0a820de6e767ffa9f887a0ac63e8
Joseph Davis <joethebartenderpdx@gmail.com>
2021-04-12 14:28:17 -0700

    added custom results div on results page

--- results/results.js
+++ results/results.js
@@ -1,7 +1,17 @@
 import { getUser } from '../local-storage-utils.js';
 const user = getUser();
 // const section = document.querySelector('section');
-const results = document.querySelector('#results');
+const main = document.querySelector('main');
+const button = document.querySelector('button');
 
+const userIsDead = user.hp <= 0;
+if (!userIsDead) {
+    main.textContent = `Well you made it to the end with ${user.gold} gold in your pocket. Spend it wisely!`;
+} else {
+    main.textContent = `No amount of gold could save you from your inevitable death... Rest well you fool.`;
+}
 
-results.textContent = `HP; ${user.hp} --- Gold: ${user.gold}`;
+button.addEventListener('click', () => {
+    localStorage.clear();
+    window.location = '../index.html';
+});
